780d3db2a0cada82594868e39b6e7595a74378cb
endo-202588 <stb5181@gmail.com>
2026-02-20 18:55:33 +0900

    fix:link_to

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def top_page?
+    controller_path == "static_pages" && action_name == "top"
+  end
+
+  def hide_footer_nav?
+    (controller_path == "users" && action_name == "index") ||
+    (controller_path == "dashboard" && action_name == "show")
+  end
 end
