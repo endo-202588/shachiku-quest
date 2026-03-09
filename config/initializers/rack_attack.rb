f08d6ab148663db3bb61047196ce4a770c8a2e29
YUKI ENDO <stb5181@gmail.com>
2026-03-09 15:10:37 +0900

    add:redis

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,22 +1,11 @@
 class Rack::Attack
-  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+  Rack::Attack.cache.store = Rails.cache
 
   throttle("logins/ip_email", limit: 5, period: 60.seconds) do |req|
     if req.post? && req.path.start_with?("/login")
       email = req.params["email"]&.downcase&.strip || ""
-      "#{req.ip}:#{email}"
+      ip = req.get_header("HTTP_X_FORWARDED_FOR")&.split(",")&.first || req.ip
+      "#{ip}:#{email}"
     end
-  end
-
-  self.throttled_responder = lambda do |req|
-    if req.env["rack.session"]
-      req.env["rack.session"][:throttled] = true
-    end
-
-    [
-      302,
-      { "Location" => "/login" },
-      []
-    ]
   end
 end
