743a531dee8eff2b778095478717aa5d785e4286
YUKI ENDO <stb5181@gmail.com>
2026-03-09 16:21:35 +0900

    fix sorcery reset_password_mailer config

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -6,8 +6,7 @@ Rails.application.config.sorcery.configure do |config|
   config.user_class = "User"
 
   config.user_config do |user|
-    # reset_password 用の設定は "user" 側に書く
-    user.reset_password_mailer = UserMailer
+    user.reset_password_mailer = :user_mailer
     user.reset_password_email_method_name = :reset_password_email
     user.reset_password_time_between_emails = 60
     user.reset_password_expiration_period = 2.hours
